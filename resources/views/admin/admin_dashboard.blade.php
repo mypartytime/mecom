c91ae361de7ba79993f4f28cb3609f755ae88a52
mypartytime <140714609+mypartytime@users.noreply.github.com>
2023-07-28 16:27:06 +0700

    003 Create route for 3 type login
    1. admin
    2. user
    3. vendor

---
+++ resources/views/admin/admin_dashboard.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>admin dashboard</title>
+
+       
+    </head>
+    <body>
+
+        <h1>Admin Dashboard</h1>
+    
+    </body>
+
+</html>
